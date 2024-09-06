3a38090a0dd4adaf0e41b6ea50a9706d72b03c92
Mactavish410 <mactavish410@gmail.com>
2024-09-07 01:25:59 +0500

    Закоммитил изменения перед переключением на gh-pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "https://github.com/Mactavish410/informative-tool", // Убедитесь, что здесь указано имя вашего репозитория
+  build: {
+    rollupOptions: {
+      input: "./index.html", // Убедитесь, что у вас есть файл index.html
+    },
+  },
+  optimizeDeps: {
+    include: ["react", "react-dom"], // Явно укажите зависимости
+  },
+});
